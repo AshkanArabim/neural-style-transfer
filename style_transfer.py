f7f20c2e43a5ae418bc909ff604084d4acdd5c44
AshkanArabim <ashkan.arabim@gmail.com>
2024-01-24 22:16:42 -0700

    fix frozen loss

--- style_transfer.py
+++ style_transfer.py
@@ -19,22 +19,22 @@ def style_transfer(configs):
     
     # initialize output image (random pixels) --> we will tune this
     output_image = torch.from_numpy(np.random.rand(*content_dims))
-    output_image = torch.autograd.Variable(output_image)
+    output_image = torch.autograd.Variable(output_image, requires_grad=True)
     
     # train the output image
-    optimizer = torch.optim.Adam([output_image], lr = 1e-4)
+    optimizer = torch.optim.Adam([output_image], lr = 1e-1)
     for i in range(configs["max_iters"]):
         optimizer.zero_grad()
         output_image_preprocessed = utils.vgg_preprocessing(output_image, configs["device"])
         out_style_reps, out_content_rep = neural_net(output_image_preprocessed)
         content_loss, style_loss, loss = utils.calculate_loss(out_style_reps, out_content_rep, style_reps, content_rep, configs)
         # print("iteration:", i, "\tloss:", loss)
-        print(f"loss: {loss}\tcontent_loss: {content_loss}\tstyle_loss: {style_loss}")
+        print(f"iter {i}/{configs['max_iters']} | loss: {loss}\t\tcontent_loss: {content_loss}\t\tstyle_loss: {style_loss}")
         loss.backward(retain_graph=True)
         optimizer.step()
     
     # convert to pixel values
-    output_image = (output_image.numpy() * 255).astype(int)
+    output_image = (output_image.detach().numpy() * 255).astype(int)
     # save output
     cv.imwrite(
         # os.path.join(configs["output_dir"], f"combined_{configs['style_image_name']}_{configs['content_image_name']}"),
@@ -53,7 +53,7 @@ if __name__ == "__main__":
         "style_loss_weight": 1,
         "content_loss_weight": 1,
         "variance_loss_weight": 1,
-        "max_iters": 100, # FIXME: revert to 1000 for final training
+        "max_iters": 1000,
         # don't tweak below this line -------------
         "output_name": "out.jpg",
         # "output_dir": os.path.join(os.path.dirname(__file__), "output"),
